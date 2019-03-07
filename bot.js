bebec4a95dfbbd8f5b8b3e91228e94dbd9f16800
Azoooooooooz <44898336+epic3azoozez135@users.noreply.github.com>
2019-03-07 17:07:20 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = '%';
+const prefix = '-';
 
 client.on('message', message => {
    if(!message.channel.guild) return;
